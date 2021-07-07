4601eccbe20ae1b51d6c4391ff27d7336976e9a1
Bo Lu <bo.lu@utexas.edu>
2021-07-07 16:31:15 -0700

    add react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css';
-import './index.css';
-import {DesktopOutlined} from '@ant-design/icons' 
-import { Layout, Menu, Breadcrumb } from 'antd';
+import "./App.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+import "./index.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams,
+} from "react-router-dom";
+
+import { DesktopOutlined } from "@ant-design/icons";
+import { Layout, Menu, Breadcrumb } from "antd";
+
 const { Header, Content, Footer } = Layout;
 
 function App() {
@@ -16,7 +26,12 @@ function App() {
           <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
             {new Array(5).fill(null).map((_, index) => {
               const key = index + 1;
-              return <Menu.Item key={key} icon={<DesktopOutlined />}>{`nav ${key}`}</Menu.Item>;
+              return (
+                <Menu.Item
+                  key={key}
+                  icon={<DesktopOutlined />}
+                >{`nav ${key}`}</Menu.Item>
+              );
             })}
           </Menu>
         </Header>
@@ -27,7 +42,17 @@ function App() {
             <Breadcrumb.Item>App</Breadcrumb.Item>
           </Breadcrumb>
           <div className="site-layout-content">
-            Content
+            <Switch>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/topics">
+                <Topics />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
           </div>
         </Content>
         <Footer style={{ textAlign: "center" }}>
@@ -36,6 +61,53 @@ function App() {
       </Layout>
     </div>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Topics() {
+  let match = useRouteMatch();
+
+  return (
+    <div>
+      <h2>Topics</h2>
+
+      <ul>
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>
+            Props v. State
+          </Link>
+        </li>
+      </ul>
+
+      {/* The Topics page has its own <Switch> with more routes
+          that build on the /topics URL path. You can think of the
+          2nd <Route> here as an "index" page for all topics, or
+          the page that is shown when no topic is selected */}
+      <Switch>
+        <Route path={`${match.path}/:topicId`}>
+          <Topic />
+        </Route>
+        <Route path={match.path}>
+          <h3>Please select a topic.</h3>
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Topic() {
+  let { topicId } = useParams();
+  return <h3>Requested topic ID: {topicId}</h3>;
 }
 
 export default App;
